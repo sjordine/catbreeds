89b2a46ac3b013817538ec3cf5ca2db69d30aaf7
Sergio <sergio.ordine@eldorado.org.br>
2024-05-04 09:51:05 -0300

    Basic Cat Breed list

--- CatBreeds/CatBreeds/CatListViewController.swift
+++ CatBreeds/CatBreeds/CatListViewController.swift
@@ -11,22 +11,39 @@ class CatListViewController: UIViewController {
     
     @IBOutlet weak var catBreedList: UITableView!
     
+    var breeds:[CatBreed] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        catBreedList.dataSource = self
         Task {
             if let file = Bundle.main.url(forResource: "CatBreeds", withExtension: "json") {
-                var breeds = []
-                
+
                 let data = try Data(contentsOf: file)
                 breeds = try JSONDecoder().decode(Array<CatBreed>.self,
                                                       from: data)
-                
-                
+
                 if !breeds.isEmpty {
-                    print(breeds)
+                    catBreedList.reloadData()
                 }
             }
         }
+    }
+    
+}
+
+extension CatListViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return breeds.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CatBreedCell", for: indexPath)
+        
+        cell.textLabel?.text = breeds[indexPath.row].breed
+        
+        return cell
     }
     
     
