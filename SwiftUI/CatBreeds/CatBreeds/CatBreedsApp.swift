6f6179e01e5403e3f2f27f3bd77daf38ef94f702
Sergio <sergio.ordine@eldorado.org.br>
2024-05-09 12:28:04 -0300

    A New Beginning

---
+++ SwiftUI/CatBreeds/CatBreeds/CatBreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatBreedsApp.swift
+//  CatBreeds
+//
+//  Created by Sergio Ordine on 09/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CatBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
